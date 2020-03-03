662a930ccbe85a80384e83a389a73381e1e5226f
Wael Yehdego <wael@waels-mbp.attlocal.net>
2020-03-03 00:34:17 -0800

    Baseline synching of django and vue complete. Yeee

---
+++ vango-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/wael/Desktop/digital/adventures/vango-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ vango/frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <p class="hi-world">greeting </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    greeting: "Look at the vue."
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: -apple-system, 'Open Sans', sans-serif;
+  }
+  .hi-world {
+    display: flex;
+    background-color: darkslateblue;
+    color: white;
+    border-radius: 5px;
+    padding: 10px;
+  }
+</style>
